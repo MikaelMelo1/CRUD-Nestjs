2227c089996316dccad2d7ba58448d00499cfd2b
Mikael Melo <mikaelsilva1940@hotmail.com>
2024-07-30 15:58:09 -0300

    chore: initial commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [new User('mikael', 'melo@gmail.com')];
+  
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
